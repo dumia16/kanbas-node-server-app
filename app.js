435272ec270185f49e81ad34b04b6cebf4b87107
dumia16 <dujay77@gmail.com>
2023-11-14 18:30:55 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// const express = require("express");
+import express from "express";
+import "dotenv/config";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import AssignmentRoutes from "./Assignments/routes.js";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
