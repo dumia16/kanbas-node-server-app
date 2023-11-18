e7d947aafb1833a2014d7ba7db66e35ad78a4c30
dumia16 <dujay77@gmail.com>
2023-11-18 15:55:47 -0800

    fix the case sensitive problem

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
-import AssignmentRoutes from "./Assignments/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
 
 const app = express();
 
